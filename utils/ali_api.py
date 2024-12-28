e61d42576f409b230b08b1314acf71680700e8ed
Ddd <752042257@qq.com>
2024-12-28 22:52:56 +0800

    feat: 1688api集合

--- utils/ali_api.py
+++ utils/ali_api.py
@@ -1,5 +1,8 @@
 import hmac
 import global_params
+import time
+import requests
+
 
 def CalculateSignature(urlPath, data, shopName):
     # 构造签名因子：urlPath
@@ -25,3 +28,98 @@ def CalculateSignature(urlPath, data, shopName):
     hex_res1 = hex_res1.upper()
 
     return hex_res1
+
+
+# 交易数据获取  默认1页
+def GetTradeData(data, shopName):
+    data["access_token"] = global_params.access_token[shopName]
+    _aop_signature = CalculateSignature(
+        global_params.request_type["trade"] + "alibaba.trade.getSellerOrderList/" + global_params.AppKey[shopName],
+        data,
+        shopName,
+    )
+    data["_aop_signature"] = _aop_signature
+    url = (
+            global_params.base_url
+            + global_params.request_type["trade"]
+            + "alibaba.trade.getSellerOrderList/"
+            + global_params.AppKey[shopName]
+    )
+    try:
+        time.sleep(0.2)
+        res = requests.post(url, data=data)
+    except Exception as e:
+        print("post error ", e)
+
+    return res.json()
+
+
+# 已发货物流信息
+def GetDeliveryData(data, shopName):
+    data["access_token"] = global_params.access_token[shopName]
+    _aop_signature = CalculateSignature(
+        global_params.request_type["delivery"]
+        + "alibaba.trade.getLogisticsInfos.sellerView/"
+        + global_params.AppKey[shopName],
+        data,
+        shopName,
+    )
+    data["_aop_signature"] = _aop_signature
+
+    url = (
+            global_params.base_url
+            + global_params.request_type["delivery"]
+            + "alibaba.trade.getLogisticsInfos.sellerView/"
+            + global_params.AppKey[shopName]
+    )
+
+    res = requests.post(url, data=data)
+
+    return res.json()
+
+
+# 已发货物流信息+单号信息
+def GetDeliveryTraceData(data, shopName):
+    data["access_token"] = global_params.access_token[shopName]
+    tmp = CalculateSignature(
+        "param2/1/com.alibaba.logistics/alibaba.trade.getLogisticsInfos.sellerView/"
+        + global_params.AppKey[shopName],
+        data,
+        shopName,
+    )
+    _aop_signature = CalculateSignature(
+        global_params.request_type["delivery"]
+        + "alibaba.trade.getLogisticsTraceInfo.sellerView/"
+        + global_params.AppKey[shopName],
+        data,
+        shopName,
+    )
+    data["_aop_signature"] = _aop_signature
+    url = (
+            global_params.base_url
+            + global_params.request_type["delivery"]
+            + "alibaba.trade.getLogisticsTraceInfo.sellerView/"
+            + global_params.AppKey[shopName]
+    )
+
+    response = requests.post(url, data=data)
+
+    return response.json()
+
+def GetSingleTradeData(data, shopName):
+    data["access_token"] = global_params.access_token[shopName]
+    _aop_signature = CalculateSignature(
+        global_params.request_type["trade"] + "alibaba.trade.get.sellerView/" + global_params.AppKey[shopName],
+        data,
+        shopName,
+    )
+    data["_aop_signature"] = _aop_signature
+    url = (
+            global_params.base_url
+            + global_params.request_type["trade"]
+            + "alibaba.trade.get.sellerView/"
+            + global_params.AppKey[shopName]
+    )
+    res = requests.post(url, data=data)
+
+    return res.json()
