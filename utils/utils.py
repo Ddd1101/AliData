e3ae65b517e42f834398eecfd0455702d95889fb
Ddd <752042257@qq.com>
2024-12-28 21:30:14 +0800

    feat: 工具集

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,76 @@
-class Utils:
-    def __init__(self, path):
-        """初始化 Person 对象"""
-        self.name = path
-    def greet(self):
-        """打印问候语"""
-        return f"你好，我叫 {self.name}，我 {self.age} 岁。"
+import global_params
 
-    def have_birthday(self):
-        """年龄加一"""
-        self.age += 1
-        print(f"生日快乐！现在你 {self.age} 岁了。")
+
+def SplitChineseAndPinyin(shopNameRaw):
+    """
+        拆分中文和英文
+    """
+    ret = -1
+    for i in range(len(str)):
+        if (str[i] >= "a" and str[i] <= "z") or (
+                str[i] >= "A" and str[i] <= "Z"
+        ):
+            break
+        ret = i
+    return ret
+
+
+def NumFormate4Print(numStr):
+    """
+        规格化尺码
+    """
+    res = ""
+    if numStr[0] in global_params.en_code:
+        for item in numStr:
+            if item in global_params.en_code:
+                res += item
+            else:
+                break
+        fomateSpaceNum = 5 - len(res)
+        for k in range(fomateSpaceNum):
+            res = " " + res
+    else:
+        for item in numStr:
+            if item >= "0" and item <= "9":
+                res += item
+            else:
+                if len(res) and res[0] == "9":
+                    res += " "
+                break
+        res += "cm"
+    return res
+
+
+def CalPriceLocationENCode(_size):
+    """
+        对英文码进行规格化处理
+    """
+    if _size == "":
+        return -1
+
+    if _size[0] == "S" or _size[0] == "s":
+        return 24
+
+    if _size[0] == "M" or _size[0] == "m":
+        return 25
+
+    if _size[0] == "L" or _size[0] == "l":
+        return 26
+
+    if _size[0] == "x" or _size[0] == "X":
+        if _size[1] == "L" or _size[1] == "l":
+            return 27
+        elif _size[1] == "x" or _size[1] == "X":
+            if _size[2] == "L" or _size[2] == "l":
+                return 28
+            elif _size[2] == "x" or _size[2] == "X":
+                return 29
+
+    return -1
+
+
+
+def CalPageNum(totalRecord):
+    return math.ceil(totalRecord / 20)
+
+
