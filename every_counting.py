6b40031da7c5c712817ab09483c0f4760312ef71
Ddd <752042257@qq.com>
2024-12-29 22:31:19 +0800

    update: 每日核算入口

--- every_counting.py
+++ every_counting.py
@@ -1,8 +1,17 @@
 from datetime import datetime, date, timedelta
 
+from manager.cloth_trade_manager import ClothTradeManager
+
 if __name__ == "__main__":
     todayTmp = datetime.strptime(str(date.today()), "%Y-%m-%d")
-    startDateTimeTmp = todayTmp + timedelta(days=-1)
-    endDateTimeTmp = todayTmp + timedelta(days=0)
+    startDateTime = todayTmp + timedelta(days=-1)
+    endDateTime = todayTmp + timedelta(days=0)
+    cloth_trade_manager = ClothTradeManager()
 
-    print(startDateTimeTmp, endDateTimeTmp)
+    shop_names = ["联球制衣厂", "朝雄制衣厂"]
+    order_status = []
+    filter_tags = []
+    mode = []
+    cloth_trade_manager.set_params(startDateTime, endDateTime, shop_names, order_status, filter_tags)
+
+    cloth_trade_manager.start()
